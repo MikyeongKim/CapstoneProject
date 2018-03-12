698fe11a85778e34727d90f75c167f50009233ce
jonghwa <jonghwa0710@naver.com>
2018-03-13 01:15:07 +0900

    board 1st complete

--- routes/community.js
+++ routes/community.js
@@ -6,31 +6,36 @@ const category_num = 3;
 
 router.route('/').get((req, res) => {
 
-  if(! req.session.userinfo) {
+  if (!req.session.userinfo) {
     res.redirect('/');
   }
-  
+
   models.Board.findAll({
-    where: { board_category : category_num }
+    where: { board_category: category_num }
   }).then(function (result) {
-    res.render('common/community', { data : result});
+    res.render('common/community', { data: result });
   });
 });
 
-router.route('/read/:id').get((req, res) => { 
+router.route('/read/:id').get((req, res) => {
   //TODO : 세션 검사 및 조회수 및 게시판 카테고리 출력
 
   models.Board.find({
-    where: { board_no : req.params.id }
+    where: { board_no: req.params.id }
   }).then(function (result) {
-    res.render('common/boardread', { data : result});
+    if (result.user_id == req.session.userinfo[0]) {
+      return res.render('common/boardread', { data: result, writer: true });
+    }
+
+    res.render('common/boardread', { data: result, writer: false });
   });
 });
 
-router.route('/delete').get((req, res) => { });
+
 
 router.route('/insert')
   .get((req, res) => {
+
     if (!req.session.userinfo) {
       res.redirect('/');
     }
@@ -46,15 +51,15 @@ router.route('/insert')
     const today = new Date();
 
     models.Board.create({
-      board_category : req.body.category,
+      board_category: req.body.category,
       board_title: req.body.title,
       board_content: req.body.content,
-      board_department : req.body.board_department, 
+      board_department: req.body.board_department,
       user_id: req.session.userinfo[0],
     }).then(function (result) {
       console.dir(result);
       res.redirect('/community');
-    }).catch(function (err) {
+    }).catch((err) => {
       //TODO : 글작성 실패시 작성내용 쿠키에 저장
       console.dir(err);
       res.send("<html><body><script>alert('글 작성 실패');</script></body>");
@@ -63,8 +68,68 @@ router.route('/insert')
 
 
 router.route('/edit/:id')
-  .get((req, res) => { res.render('common/boardedit')})
-  .post((req, res) => { });
+  .get((req, res) => {
+
+    models.Board.find({
+      where: { board_no: req.params.id }
+    }).then(function (result) {
+
+      if (!req.session.userinfo) {
+        //todo : 잘못된 경로로 접근했습니다. 오류메세지 출력
+        return res.redirect('/community');
+      }
+
+      if (result.user_id != req.session.userinfo[0]) {
+        //todo : 잘못된 경로로 접근했습니다. 오류메세지 출력
+        return res.redirect('/community');
+      }
+
+      res.render('common/boardedit', { data: result });
+    }).catch((err) => {
+      //TODO : 글이존재하지않을경우.. 잘못된 경로접근!!
+      return res.redirect('/community');
+    });
+
+  })
+  .post((req, res) => {
+
+    const paramId = req.params.id;
+    const body = req.body;
+
+    models.Board.update({
+      baord_title: body.title, board_content: body.content
+      , board_department: body.board_department
+    }
+      , {
+        where: { board_no: paramId }
+      }).then(function (result) {
+        res.redirect('/community/read/' + paramId);
+      }).catch(function (err) {
+        //TODO: error handling
+      });
+  });
+
+router.route('/delete/:id').get((req, res) => {
+
+  models.Board.find({
+    where: { board_no: req.params.id }
+  }).then(function (result) {
+    if (result.user_id != req.session.userinfo[0]) {
+      //TODO : 잘못된 접근.. 
+      return render('/');
+    }
+
+    models.Board.destroy({
+      where: { board_no: req.params.id }
+    }).then(function (result) {
+      res.redirect('/community/');
+    }).catch(function (err) {
+      //TODO: error handling
+    });
+
+  });
+
+});
 
 
 module.exports = router;
